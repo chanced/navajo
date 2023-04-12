537a5121884b1361f66cadcacc09b09f0065a24a
Chance <chanceusc@gmail.com>
2023-04-12 12:54:02 -0400

    setting up xtask

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(name = "xtask")]
+#[command(about = "navajo tasks", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
 }
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    TestAll(TestAll),
+}
+
+#[derive(Debug, Parser)]
+struct TestAll {
+    #[arg(value_name = "VERBOSE", short = 'v', long = "verbose")]
+    verbose: bool,
+
+    /// Packege to test, defaults to workspace if not specified
+    #[arg(value_name = "PACKAGE", short = 'p', long = "package")]
+    package: Option<String>,
+}
+
+fn main() {}
