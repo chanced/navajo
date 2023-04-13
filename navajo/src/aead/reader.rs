f040556bb1f2eb25aa962155783c3aeeb0cadc07
Chance <chanceusc@gmail.com>
2023-04-12 20:16:10 -0400

    fix: aead::DecryptReader

--- navajo/src/aead/reader.rs
+++ navajo/src/aead/reader.rs
@@ -16,7 +16,7 @@ where
     reader: R,
     _marker: PhantomData<C>,
     aad: Aad<A>,
-    deserializer: Option<Decryptor<C, Vec<u8>>>,
+    decryptor: Option<Decryptor<C, Vec<u8>>>,
     buffer: VecDeque<u8>,
 }
 impl<R, A, C> DecryptReader<R, A, C>
@@ -30,7 +30,7 @@ where
             reader,
             _marker: PhantomData,
             aad,
-            deserializer: Some(Decryptor::new(cipher, Vec::new())),
+            decryptor: Some(Decryptor::new(cipher, Vec::new())),
             buffer: VecDeque::new(),
         }
     }
@@ -64,57 +64,61 @@ where
                 ctr = i + 1;
             }
         }
+        // if the amount read equals the buffer size, return the amount read
         if ctr == buf.len() {
             return Ok(ctr);
         }
 
-        if self.deserializer.is_none() {
+        // if the decryptor is none, we have already finished below
+        // so return the current byte counter
+        if self.decryptor.is_none() {
             return Ok(ctr);
         }
-        let mut deserializer = self.deserializer.take().unwrap();
+        let mut decryptor = self.decryptor.take().unwrap();
         let mut offset = 0;
-        if deserializer.method().is_none() {
+        if decryptor.method().is_none() {
             let mut method_byte = [0u8; 1];
             let n = self.reader.read(&mut method_byte)?;
             if n == 0 {
                 return Ok(ctr);
             }
-            deserializer.update(Aad(self.aad.as_ref()), &method_byte)?;
+            decryptor.update(Aad(self.aad.as_ref()), &method_byte)?;
             offset = 1;
         }
 
-        if deserializer.method().is_none() {
+        if decryptor.method().is_none() {
             return Err(DecryptError::Unspecified.into());
         }
-        match deserializer.method().unwrap() {
+        match decryptor.method().unwrap() {
             crate::aead::Method::Online => {
                 let mut data: Vec<u8> = Vec::new();
                 self.reader.read_to_end(&mut data)?;
-                let result = deserializer.finalize(Aad(self.aad.as_ref()))?;
+                decryptor.update(Aad(self.aad.as_ref()), &data)?;
+                let result = decryptor.finalize(Aad(self.aad.as_ref()))?;
                 Ok(self.update(ctr, result.flatten(), buf))
             }
             crate::aead::Method::StreamingHmacSha256(segment) => {
                 let mut data: Vec<u8> = vec![0u8; segment - offset];
                 let n = self.reader.read(&mut data)?;
-                deserializer.update(Aad(self.aad.as_ref()), &data[..n])?;
+                decryptor.update(Aad(self.aad.as_ref()), &data[..n])?;
                 if n < segment - offset {
                     let result = self.update(
                         ctr,
-                        deserializer.finalize(Aad(self.aad.as_ref()))?.flatten(),
+                        decryptor.finalize(Aad(self.aad.as_ref()))?.flatten(),
                         buf,
                     );
                     Ok(result)
                 } else {
                     let result = self.update(
                         ctr,
-                        deserializer
+                        decryptor
                             .next(Aad(self.aad.as_ref()))?
                             .unwrap()
                             .iter()
                             .cloned(),
                         buf,
                     );
-                    self.deserializer = Some(deserializer);
+                    self.decryptor = Some(decryptor);
                     Ok(result)
                 }
             }
@@ -149,5 +153,27 @@ mod tests {
         let mut buf = Vec::new();
         reader.read_to_end(&mut buf).unwrap();
         assert_eq!(data.len(), buf.len());
+        assert_eq!(data, buf)
+    }
+
+    #[test]
+    fn test_read_under_segment() {
+        let mut data = vec![0u8; 1024];
+        let rng = SystemRng::new();
+        rng.fill(&mut data).unwrap();
+        let aead = Aead::new(Algorithm::ChaCha20Poly1305, None);
+
+        let mut encryptor = Encryptor::new(rng, &aead, Some(Segment::FourKilobytes), Vec::new());
+        encryptor.update(Aad::empty(), &data).unwrap();
+        let ciphertext = encryptor
+            .finalize(Aad::empty())
+            .unwrap()
+            .flatten()
+            .collect::<Vec<u8>>();
+        let mut reader = DecryptReader::new(&ciphertext[..], Aad::empty(), &aead);
+        let mut buf = Vec::new();
+        reader.read_to_end(&mut buf).unwrap();
+        assert_eq!(data.len(), buf.len());
+        assert_eq!(data, buf)
     }
 }
